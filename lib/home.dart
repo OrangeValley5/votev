4c1fdf3d7e9120cab2b66cec940c33cf31864617
Solomon <andywlt78@gmail.com>
2024-07-09 05:09:22 +0100

    u

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -15,18 +16,49 @@ class _HomeState extends State<Home> {
       body: Container(
         padding: const EdgeInsets.only(left: 25, top: 50, right: 25),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Votex',
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+                Row(
+                  children: [Container()],
+                )
+              ],
+            ),
+            Column(
+              children: [
+                Text(
+                  '4756',
+                  style: GoogleFonts.poppins(
+                      color: Colors.white,
+                      fontSize: 34,
+                      fontWeight: FontWeight.w800),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                Image.asset(
+                  'lib/images/tapper.png',
+                  width: 150,
+                  height: 150,
+                )
+              ],
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: const [
                 Text(
                   'Votex',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
+                  style: TextStyle(color: Colors.white, fontSize: 24),
                 )
               ],
-            )
+            ),
           ],
         ),
       ),
