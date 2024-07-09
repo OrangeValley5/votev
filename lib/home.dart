60822e685751bad92f800a09272296f6167688fd
Solomon <andywlt78@gmail.com>
2024-07-09 05:56:44 +0100

    u

--- lib/home.dart
+++ lib/home.dart
@@ -47,7 +47,7 @@ class _HomeState extends State<Home> {
                             width: 4,
                           ),
                           Text(
-                            'Upgrade',
+                            'Upgrades',
                             style: TextStyle(color: Colors.white, fontSize: 12),
                           ),
                         ],
