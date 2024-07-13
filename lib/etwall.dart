628eafc973abf3bdb2fab11ac73b65f995d40830
Solomon <andywlt78@gmail.com>
2024-07-13 18:37:02 +0100

    u

--- lib/etwall.dart
+++ lib/etwall.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:votev/remove_et.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart'; // Import the spinkit package
 
 class Etwall extends StatefulWidget {
   const Etwall({Key? key}) : super(key: key);
@@ -9,192 +11,224 @@ class Etwall extends StatefulWidget {
 }
 
 class _EtwallState extends State<Etwall> {
+  bool _isLoading = false;
+
+  void _showLoading() {
+    setState(() {
+      _isLoading = true;
+    });
+    Future.delayed(Duration(seconds: 5), () {
+      setState(() {
+        _isLoading = false;
+      });
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const RemoveEt(),
+        ),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Container(
-        padding: const EdgeInsets.only(left: 20, top: 20, right: 20),
-        alignment: Alignment.center,
-        child: Column(
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Container(
+              padding: const EdgeInsets.only(left: 20, top: 20, right: 20),
+              alignment: Alignment.center,
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: Container(
-                      width: 25,
-                      padding: const EdgeInsets.all(8),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(5),
-                          color: Color.fromARGB(255, 61, 61, 61)),
-                      child: const Center(
-                        child: Icon(
-                          Icons.arrow_back_ios,
-                          color: Colors.white,
-                          size: 16,
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: Container(
+                            width: 25,
+                            padding: const EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                color: const Color.fromARGB(255, 61, 61, 61)),
+                            child: const Center(
+                              child: Icon(
+                                Icons.arrow_back_ios,
+                                color: Colors.white,
+                                size: 16,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        const Text(
+                          'ETH Balance',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w400),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text(
+                              '0.0',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                            Image.asset(
+                              'lib/images/eth2.png',
+                              width: 30,
+                              height: 30,
+                            ),
+                          ],
+                        ),
+                        const Text(
+                          '\$0.00',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w300),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: Container(
+                            height: 50,
+                            padding: const EdgeInsets.all(4),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8),
+                                color: const Color.fromARGB(255, 52, 52, 52)),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.asset(
+                                  'lib/images/up.png',
+                                  width: 16,
+                                  height: 16,
+                                ),
+                                const SizedBox(
+                                  width: 4,
+                                ),
+                                const Text(
+                                  'Deposit',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 12),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
                       ),
-                    ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: () {
+                            _showLoading();
+                          },
+                          child: Container(
+                            height: 50,
+                            padding: const EdgeInsets.all(4),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8),
+                                color: const Color.fromARGB(255, 52, 52, 52)),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.asset(
+                                  'lib/images/down.png',
+                                  width: 16,
+                                  height: 16,
+                                ),
+                                const SizedBox(
+                                  width: 4,
+                                ),
+                                const Text(
+                                  'Withdraw',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 12),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                   const SizedBox(
                     height: 20,
                   ),
-                  const Text(
-                    'ETH Balance',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 12,
-                        fontWeight: FontWeight.w400),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  Column(
                     children: [
-                      const Text(
-                        '0.0',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 30,
-                            fontWeight: FontWeight.w700),
+                      const SizedBox(
+                        height: 50,
                       ),
                       Image.asset(
-                        'lib/images/eth2.png',
+                        'lib/images/voltscancel.png',
                         width: 30,
                         height: 30,
                       ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Text(
+                        'No Transactions',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w700),
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      const Text(
+                        'You are yet to make a transaction. Any transaction made will show up here',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Color(0xFF8A8A8A),
+                            fontSize: 10,
+                            fontWeight: FontWeight.w300),
+                      ),
                     ],
-                  ),
-                  const Text(
-                    '\$0.00',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 12,
-                        fontWeight: FontWeight.w300),
-                  ),
+                  )
                 ],
               ),
             ),
-            const SizedBox(
-              height: 50,
+          ),
+          if (_isLoading)
+            Container(
+              color: Colors.black54,
+              child: const Center(
+                child: SpinKitFadingCircle(
+                  color: Color.fromARGB(255, 27, 255, 91),
+                  size: 50.0,
+                ),
+              ),
             ),
-            Row(
-              children: [
-                Expanded(
-                  child: GestureDetector(
-                    onTap: () {},
-                    child: Container(
-                      height: 50,
-                      padding: const EdgeInsets.all(4),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(8),
-                          color: Color.fromARGB(255, 52, 52, 52)),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset(
-                            'lib/images/up.png',
-                            width: 16,
-                            height: 16,
-                          ),
-                          const SizedBox(
-                            width: 4,
-                          ),
-                          const Text(
-                            'Deposit',
-                            style: TextStyle(color: Colors.white, fontSize: 12),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                Expanded(
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const RemoveEt(),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      height: 50,
-                      padding: const EdgeInsets.all(4),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(8),
-                          color: Color.fromARGB(255, 52, 52, 52)),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset(
-                            'lib/images/down.png',
-                            width: 16,
-                            height: 16,
-                          ),
-                          const SizedBox(
-                            width: 4,
-                          ),
-                          const Text(
-                            'Withdraw',
-                            style: TextStyle(color: Colors.white, fontSize: 12),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Column(
-              children: [
-                const SizedBox(
-                  height: 50,
-                ),
-                Image.asset(
-                  'lib/images/voltscancel.png',
-                  width: 30,
-                  height: 30,
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const Text(
-                  'No Transactions',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w700),
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                const Text(
-                  'You are yet to make a transaction. Any transaction made will show up here',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Color(0xFF8A8A8A),
-                      fontSize: 10,
-                      fontWeight: FontWeight.w300),
-                ),
-              ],
-            )
-          ],
-        ),
+        ],
       ),
     );
   }
