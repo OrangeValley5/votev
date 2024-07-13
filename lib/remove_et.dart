8b2173d154649395661e17f73257ed92118ffbba
Solomon <andywlt78@gmail.com>
2024-07-13 16:18:22 +0100

    u

--- lib/remove_et.dart
+++ lib/remove_et.dart
@@ -30,22 +30,74 @@ class _RemoveEtState extends State<RemoveEt> {
                 ),
                 const Text(
                   'Withdraw ETH',
-                  style: TextStyle(color: Colors.white),
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.w500),
                 )
               ],
             ),
             const SizedBox(
-              height: 20,
+              height: 80,
+            ),
+            Row(
+              children: const [
+                Text(
+                  'Address',
+                  style: TextStyle(color: Colors.white, fontSize: 12),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 5,
             ),
             Container(
-              padding: const EdgeInsets.all(0),
+              padding: const EdgeInsets.only(left: 10, right: 10),
               width: MediaQuery.of(context).size.width,
-              height: 50,
+              height: 45,
               decoration: BoxDecoration(
                   color: Color.fromARGB(255, 60, 60, 60),
-                  borderRadius: BorderRadius.circular(20)),
+                  borderRadius: BorderRadius.circular(10)),
               child: TextFormField(
-                style: const TextStyle(fontSize: 14, color: Colors.white),
+                style: const TextStyle(fontSize: 12, color: Colors.white),
+                decoration: const InputDecoration(
+                  hintText: 'Enter wallet address',
+                  enabledBorder:
+                      InputBorder.none, // Remove underline when enabled
+                  focusedBorder:
+                      InputBorder.none, // Remove underline when focused
+                  hintStyle: TextStyle(
+                      fontSize: 12, color: Color.fromARGB(255, 203, 203, 203)),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text(
+                  'Amount',
+                  style: TextStyle(color: Colors.white, fontSize: 12),
+                ),
+                Text(
+                  'Available: 0.00 ETH',
+                  style: TextStyle(
+                      color: Color.fromARGB(255, 175, 175, 175), fontSize: 10),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Container(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              width: MediaQuery.of(context).size.width,
+              height: 45,
+              decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 60, 60, 60),
+                  borderRadius: BorderRadius.circular(10)),
+              child: TextFormField(
+                style: const TextStyle(fontSize: 12, color: Colors.white),
                 decoration: const InputDecoration(
                   hintText: 'Enter amount',
                   enabledBorder:
@@ -53,8 +105,68 @@ class _RemoveEtState extends State<RemoveEt> {
                   focusedBorder:
                       InputBorder.none, // Remove underline when focused
                   hintStyle: TextStyle(
-                      fontSize: 14, color: Color.fromARGB(255, 203, 203, 203)),
+                      fontSize: 12, color: Color.fromARGB(255, 203, 203, 203)),
                 ),
+              ),
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            Row(
+              children: const [
+                Text(
+                  'Dollar Equivalent: ',
+                  style: TextStyle(color: Colors.white, fontSize: 8),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              padding:
+                  const EdgeInsets.only(top: 12, bottom: 12, right: 8, left: 8),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Color.fromARGB(255, 69, 255, 100)
+                    .withOpacity(0.2), // Adjust opacity here
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: const [
+                  Text(
+                    'Make sure you deposit your tokens to the right address and network',
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 8, 255, 70),
+                        fontSize: 10,
+                        fontWeight: FontWeight.w300),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 100,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              padding:
+                  const EdgeInsets.only(top: 15, bottom: 15, right: 8, left: 8),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Color.fromARGB(255, 69, 255, 100)
+                    .withOpacity(1), // Adjust opacity here
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: const [
+                  Text(
+                    'Withdraw',
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        fontSize: 12,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ],
               ),
             ),
           ],
