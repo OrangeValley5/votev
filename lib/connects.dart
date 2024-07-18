18510fc62d54c0e63f2de4071ac02b67d479515f
Solomon <andywlt78@gmail.com>
2024-07-18 07:04:59 +0100

    u

--- lib/connects.dart
+++ lib/connects.dart
@@ -10,93 +10,132 @@ class Connets extends StatefulWidget {
   State<Connets> createState() => _ConnetsState();
 }
 
-class _ConnetsState extends State<Connets> {
+class _ConnetsState extends State<Connets> with TickerProviderStateMixin {
+  late AnimationController _slideController;
+  late Animation<Offset> _offsetAnimation;
+  late AnimationController _rotationController;
+
   @override
   void initState() {
     super.initState();
+
+    _slideController = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    );
+    _offsetAnimation = Tween<Offset>(
+      begin: const Offset(0.0, 1.0),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(
+      parent: _slideController,
+      curve: Curves.easeInOut,
+    ));
+
+    _rotationController = AnimationController(
+      duration: const Duration(seconds: 5),
+      vsync: this,
+    )..repeat();
+
     _showWelcomeDialog();
+  }
+
+  @override
+  void dispose() {
+    _slideController.dispose();
+    _rotationController.dispose();
+    super.dispose();
   }
 
   void _showWelcomeDialog() {
     Future.delayed(Duration.zero, () {
+      _slideController.forward();
       showDialog(
         context: context,
+        barrierDismissible: true,
         builder: (BuildContext context) {
-          return Padding(
-            padding: const EdgeInsets.all(30.0),
-            child: Dialog(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0),
-                side: const BorderSide(
-                  color: Color.fromARGB(255, 50, 255, 122),
-                  width: 1.0,
-                ), // Border color and width
-              ),
-              backgroundColor: Color.fromARGB(
-                  255, 24, 24, 24), // Example of custom background color
-              child: Container(
-                padding: const EdgeInsets.all(20),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min, // To constrain the height
-                  children: [
-                    Container(
-                        width: 80,
-                        height: 80,
-                        child: Image.asset('lib/images/usdt.png')),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    const Text(
-                      'Congratulations !',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold,
+          return SlideTransition(
+            position: _offsetAnimation,
+            child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Dialog(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                  side: const BorderSide(
+                    color: Color.fromARGB(255, 51, 51, 51),
+                    width: 1.0,
+                  ),
+                ),
+                backgroundColor: Color.fromARGB(255, 24, 24, 24),
+                child: Container(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      RotationTransition(
+                        turns: _rotationController,
+                        child: Container(
+                          width: 80,
+                          height: 80,
+                          child: Image.asset('lib/images/usdt.png'),
+                        ),
                       ),
-                    ),
-                    const SizedBox(height: 10),
-                    const Text(
-                      'Yay ! you just earned 10 usdt for joining. We are giving out 10 usdt to the first 1000 players who join the volts telegram game. You can view this in your wallet',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Text(
+                        'Congratulations!',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      const Text(
+                        'Yay! You just earned 10 USDT for joining. We are giving out 10 USDT to the first 1000 players who join the volts telegram game. You can view this in your wallet.',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
                           color: Color.fromARGB(255, 183, 183, 183),
                           fontSize: 10,
-                          fontWeight: FontWeight.w300),
-                    ),
-                    const SizedBox(height: 30),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            padding: const EdgeInsets.symmetric(
-                              vertical: 10,
-                              horizontal: 20,
-                            ),
-                            child: const Text(
-                              'Continue',
-                              style: TextStyle(
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 10,
+                                horizontal: 20,
+                              ),
+                              child: const Text(
+                                'Continue',
+                                style: TextStyle(
                                   fontSize: 12,
                                   color: Color.fromARGB(255, 255, 255, 255),
-                                  fontWeight: FontWeight.w500),
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
           );
         },
-      );
+      ).then((_) => _slideController.reset());
     });
   }
 
@@ -115,9 +154,10 @@ class _ConnetsState extends State<Connets> {
             const Text(
               'Wallet',
               style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                  fontWeight: FontWeight.w700),
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.w700,
+              ),
             ),
             const SizedBox(
               height: 50,
@@ -127,9 +167,10 @@ class _ConnetsState extends State<Connets> {
                 Text(
                   'Total Balance',
                   style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 12,
-                      fontWeight: FontWeight.w400),
+                    color: Colors.white,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                  ),
                 ),
                 SizedBox(
                   height: 10,
@@ -137,9 +178,10 @@ class _ConnetsState extends State<Connets> {
                 Text(
                   '\$0.00',
                   style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 40,
-                      fontWeight: FontWeight.w700),
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
               ],
             ),
@@ -162,8 +204,9 @@ class _ConnetsState extends State<Connets> {
                       height: 50,
                       padding: const EdgeInsets.all(4),
                       decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(8),
-                          color: Color.fromARGB(255, 52, 52, 52)),
+                        borderRadius: BorderRadius.circular(8),
+                        color: Color.fromARGB(255, 52, 52, 52),
+                      ),
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
@@ -177,7 +220,10 @@ class _ConnetsState extends State<Connets> {
                           ),
                           const Text(
                             'Deposit',
-                            style: TextStyle(color: Colors.white, fontSize: 12),
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                            ),
                           ),
                         ],
                       ),
@@ -192,8 +238,9 @@ class _ConnetsState extends State<Connets> {
                     height: 50,
                     padding: const EdgeInsets.all(4),
                     decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        color: Color.fromARGB(255, 52, 52, 52)),
+                      borderRadius: BorderRadius.circular(8),
+                      color: Color.fromARGB(255, 52, 52, 52),
+                    ),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
@@ -207,7 +254,10 @@ class _ConnetsState extends State<Connets> {
                         ),
                         const Text(
                           'Withdraw',
-                          style: TextStyle(color: Colors.white, fontSize: 12),
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 12,
+                          ),
                         ),
                       ],
                     ),
@@ -233,8 +283,9 @@ class _ConnetsState extends State<Connets> {
                     height: 50,
                     padding: const EdgeInsets.all(10),
                     decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        color: Color.fromARGB(255, 52, 52, 52)),
+                      borderRadius: BorderRadius.circular(8),
+                      color: Color.fromARGB(255, 52, 52, 52),
+                    ),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
@@ -250,17 +301,20 @@ class _ConnetsState extends State<Connets> {
                             ),
                             const Text(
                               'VOTEX',
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 12),
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 12,
+                              ),
                             ),
                           ],
                         ),
                         const Text(
                           '0.00',
                           style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 12,
-                              fontWeight: FontWeight.w500),
+                            color: Colors.white,
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                          ),
                         ),
                       ],
                     ),
@@ -282,8 +336,9 @@ class _ConnetsState extends State<Connets> {
                     height: 50,
                     padding: const EdgeInsets.all(10),
                     decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        color: const Color.fromARGB(255, 52, 52, 52)),
+                      borderRadius: BorderRadius.circular(8),
+                      color: const Color.fromARGB(255, 52, 52, 52),
+                    ),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
@@ -299,24 +354,27 @@ class _ConnetsState extends State<Connets> {
                             ),
                             const Text(
                               'ETHEREUM',
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 12),
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 12,
+                              ),
                             ),
                           ],
                         ),
                         const Text(
                           '0.00',
                           style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 12,
-                              fontWeight: FontWeight.w500),
+                            color: Colors.white,
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                          ),
                         ),
                       ],
                     ),
                   ),
-                )
+                ),
               ],
-            )
+            ),
           ],
         ),
       ),
